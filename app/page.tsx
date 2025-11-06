0cf3585a4bec68068a600b3529c7d2d9a7431dca
choirock <mccchalla@gmail.com>
2025-11-06 23:12:17 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CHOIROCK</h1>
+      <Link href="https://blog.naver.com/eric9800/224067171845">졸업작품 감상하기</Link>
+    </main>
+  )
+}
